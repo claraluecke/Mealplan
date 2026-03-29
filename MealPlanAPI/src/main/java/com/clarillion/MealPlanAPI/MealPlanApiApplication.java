15d59648860e2292bc536a134ef69a0fc488a009
Clara Timo Lücke <tluecke42@gmail.com>
2026-03-29 11:23:39 +0200

    Adds initial Skeleton spring boot project for the backend API

---
+++ MealPlanAPI/src/main/java/com/clarillion/MealPlanAPI/MealPlanApiApplication.java
@@ -0,0 +1,13 @@
+package com.clarillion.MealPlanAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlanApiApplication.class, args);
+	}
+
+}
---
+++ MealPlanAPI/src/test/java/com/clarillion/MealPlanAPI/MealPlanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarillion.MealPlanAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
